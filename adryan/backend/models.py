4092f4a2c33a7ce6cb3470a81a44200276a8a0dd
Adryan1928 <adryanoliveira1928@gmail.com>
2025-09-08 13:19:42 -0300

    Uma parte não foi

--- adryan/backend/models.py
+++ adryan/backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, rela
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
